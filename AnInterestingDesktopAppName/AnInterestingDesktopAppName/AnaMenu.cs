9ca9c4b482d37079d02f8bf30c7def85076c7ccc
Arda Eren <Poedx2@gmail.com>
2023-02-04 21:57:14 +0300

    initial submit

---
+++ AnInterestingDesktopAppName/AnInterestingDesktopAppName/AnaMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnInterestingDesktopAppName
+{
+    public partial class AnaMenu : Form
+    {
+        public AnaMenu()
+        {
+            GirisEkrani ge = new GirisEkrani();
+            ge.ShowDialog();
+            InitializeComponent();
+            label1.Text = "a";
+        }
+
+        private void AnaMenu_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ AnInterestingDesktopAppName/AnInterestingDesktopAppName/GirisEkrani.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AnInterestingDesktopAppName
+{
+    partial class GirisEkrani
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_Mail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_Sifre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_Mail
+            // 
+            this.tb_Mail.Location = new System.Drawing.Point(53, 12);
+            this.tb_Mail.Name = "tb_Mail";
+            this.tb_Mail.Size = new System.Drawing.Size(169, 20);
+            this.tb_Mail.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mail:";
+            // 
+            // tb_Sifre
+            // 
+            this.tb_Sifre.Location = new System.Drawing.Point(53, 38);
+            this.tb_Sifre.Name = "tb_Sifre";
+            this.tb_Sifre.Size = new System.Drawing.Size(169, 20);
+            this.tb_Sifre.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // GirisEkrani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 111);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Sifre);
+            this.Controls.Add(this.tb_Mail);
+            this.Name = "GirisEkrani";
+            this.Text = "GirisEkrani";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.GirisEkrani_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Mail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_Sifre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
